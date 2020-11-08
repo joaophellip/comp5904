642ae6643e8bee32ce1b845e60510b94d0adfb6c
Joao Mello <joaophellip@cozo.com.br>
2020-11-07 23:43:43 -0300

    updated README with link for report
    added boxplot to show range of predicted values in lm

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ código para competição da disciplina MAE5904
 ## problema
 modeling earthquake damage : [link aqui](https://www.drivendata.org/competitions/57/nepal-earthquake/)
 
+## relatório
+O relatório com o detalhamento dos métodos e discussões sobre os resultados pode ser consultado [aqui](https://docs.google.com/document/d/15c1aUz4iZqGZI0TAWmVIH_WEBB1Mw9esFBzNu6KwDro/edit?usp=sharing)
+
 ## estrutura das pastas
 
 * datasets : contém os conjuntos de dados repartidos em treinamento e teste. 
@@ -21,4 +24,4 @@ modeling earthquake damage : [link aqui](https://www.drivendata.org/competitions
 
 ## execução dos scripts
 
-para gerar predições para cada modelo, basta rodar os arquivos index.R dentro das pastas que identificam os modelos.
+para gerar predições para cada modelo, basta rodar os arquivos index.R dentro das pastas que identificam os modelos. Exemplo: `source('./src/models/lm/index.R')`
--- src/models/lm/index.R
+++ src/models/lm/index.R
@@ -9,6 +9,7 @@ data$generate()
 
 cat("--fitting regression models for each response indicator variable----\n")
 
+# dropping some variables from dataset in order to speed up execution time
 drops <- c("geo_level_2_id", "geo_level_3_id")
 
 lm_low_damage <- lm(data$indicator_response$low_damage ~ ., data$dataset[, !(names(data$dataset) %in% drops)])
@@ -18,8 +19,17 @@ lm_severe_damage <- lm(data$indicator_response$severe_damage ~ ., data$dataset[,
 lm_predictions <- data.frame(lm_low_damage$fitted.values, lm_medium_damage$fitted.values,
                             lm_severe_damage$fitted.values)
 
+# classifying to the class with largest fit, e.g. the higher estimated conditional probability
 training_classification <- as.integer(apply(lm_predictions, 1, which.max))
 
+# calculating in sample performance by taking the hit ratio (number of correct classifications over the total number of samples)
 in_sample_performance <- table(training_classification == data$class_response)["TRUE"] / length(training_classification)
+
+# plotting ranges for all three class predictions
+boxplot(lm_predictions$lm_low_damage.fitted.values, lm_predictions$lm_medium_damage.fitted.values, 
+        lm_predictions$lm_severe_damage.fitted.values, names = c("low damage", "medium damage", "severe damage"))
+title("Estimated conditional probability")
+
+# todo: add graph with boundary (points with same probability for all classes) 
 
 rm(list = c("drops", "lm_low_damage", "lm_medium_damage", "lm_severe_damage"))
